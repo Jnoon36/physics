cb87bff84cf98d350ed75ee4518c95312b5c0eb1
Jacob <Jacob.noon@rockets.utoledo.edu>
2019-01-30 08:23:34 -0600

    Update confined_relativistic_hydrogen.py

--- confined_relativistic_hydrogen.py
+++ confined_relativistic_hydrogen.py
@@ -14,7 +14,9 @@ bohr = .0529177  # bohr radius in nm
 
 def f(E):
     """
-    R is the confining radius in units of the bohr radius, k = +/-(1, 2, 3, ...), E is the energy in units of mc^2, Z is the amount of nucleons (will be equal to one in this case), L = (1/2)*sqrt((2*k+1)**2), kv is the modified bessel function of the second kind, V is the potential energy term (in units of mc^2), s = sqrt(k**2 - (Za)**2)
+    R is the confining radius in units of the bohr radius, k = +/-(1, 2, 3, ...), E is the energy in units of mc^2, Z is 
+    the amount of nucleons (will be equal to one in this case), L = (1/2)*sqrt((2*k+1)**2), kv is the modified bessel 
+    function of the second kind, V is the potential energy term (in units of mc^2), s = sqrt(k**2 - (Za)**2)
     """
     A = 1 + E - V
     B = 1 - E + V
@@ -33,7 +35,10 @@ list9 = []  # 3p3/2 radii, energies, k = -2
 list10 = []  # 4p3/2 radii, energies, k = -2
 
 """
-Some states you will see two for loops. This is because if we were to stretch out R futher, the function f no longer has opposite signs at the endpoints. So we need to move the endpoint b in closer to endpoint a (the unconfined binding energy) in order to find more roots and assure asymptotic behavior as R gets further away. In simpler english, moving R further out requires a slight change in the interval we hope to find roots.
+Some states you will see two for loops. This is because if we were to stretch out R futher, the function f no longer has 
+opposite signs at the endpoints. So we need to move the endpoint b in closer to endpoint a (the unconfined binding energy) 
+in order to find more roots and assure asymptotic behavior as R gets further away. In simpler english, moving R further out 
+requires a slight change in the interval we hope to find roots.
 """
 
 # k=-1 states
@@ -229,10 +234,13 @@ for i in np.arange(23, 34.7, 0.9):
     list10.append((i, bind))
 
 """
-These domains for R values were a result of trial and error. They mostly coincide with the principle quantum numbers (2s1/2 has the same ranges as the 3p1/2 and 3p3/2), but they can be slightly different (s orbitals have different shapes than p orbitals). Generally speaking, the higher the n value for ns1/2, np1/2, or np3/2, the further out R needs to be.
+These domains for R values were a result of trial and error. They mostly coincide with the principle quantum numbers 
+(2s1/2 has the same ranges as the 3p1/2 and 3p3/2), but they can be slightly different (s orbitals have different shapes than
+p orbitals). Generally speaking, the higher the n value for ns1/2, np1/2, or np3/2, the further out R needs to be.
 """
 
-# Next we need to fit polynomials (we will try for 7th degree) through each state. Before that, we must unzip each list of tuples into a list of separate R values (multiples of the bohr radius) and E (bind) values to be used for fitting.
+# Next we need to fit polynomials (we will try for 7th degree) through each state. Before that, we must unzip each list of 
+# tuples into a list of separate R values (multiples of the bohr radius) and E (bind) values to be used for fitting.
 
 R1, E1 = zip(*list1)
 R2, E2 = zip(*list2)
@@ -322,11 +330,19 @@ plt.legend()
 
 
 """
-Figures 2, 3, and 4 look like each individual plot is the same as in the other figures, but they are not. If you plot all 9 polynomials (with the data points), you will see a slight difference in radial value and energy value. This is hard to see from three separate plots, but you can go ahead and plot all 9 on the same graph to check it out yourself.
+Figures 2, 3, and 4 look like each individual plot is the same as in the other figures, but they are not. If you plot all 9 
+polynomials (with the data points), you will see a slight difference in radial value and energy value. This is hard to see 
+from three separate plots, but you can go ahead and plot all 9 on the same graph to check it out yourself.
 
-If you look up the energies for each state when the atom is unconfined (which is what each of the confined states here approach as R goes to infinity), you will notice, for example, that the 2s1/2 and 2p1/2 have the same binding energy, but are slightly lower in energy than the 2p3/2, because j = 3/2. This is a result of the spin-orbit interaction. Another point to note is that even though 2s1/2 and 3s/12 states approach the exact same energy, one approaches it faster than the other because of the shape differences betwen the p and s orbitals.
+If you look up the energies for each state when the atom is unconfined (which is what each of the confined states here 
+approach as R goes to infinity), you will notice, for example, that the 2s1/2 and 2p1/2 have the same binding energy, 
+but are slightly lower in energy than the 2p3/2, because j = 3/2. This is a result of the spin-orbit interaction. 
+Another point to note is that even though 2s1/2 and 3s/12 states approach the exact same energy, one approaches it faster 
+than the other because of the shape differences betwen the p and s orbitals.
 
-The last thing I want to plot is the comparison of ground state energies between the relativistic and non-relativistic confined hydrogen atoms with the exact same barrier height (V = 1). These numbers come from reference #4 in my paper. We will use the same R values as they do.
+The last thing I want to plot is the comparison of ground state energies between the relativistic and non-relativistic 
+confined hydrogen atoms with the exact same barrier height (V = 1). These numbers come from reference #4 in my paper. We 
+will use the same R values as they do.
 """
 
 r = [5.77827, 4.87924, 4.08889, 3.45203, 3.15412, 2.51487, 2.04918]
@@ -372,3 +388,8 @@ plt.ylabel('Binding Energy (eV)')
 plt.title('Binding Energy vs Confinement Radius (Hydrogen Ground State)')
 plt.legend()
 plt.show()
+
+"""
+For figure 5, you will notice that there is very little difference in energy for large confining radii. It is only when R
+gets to about 3*bohr radius that we start to see large differences in energy, hence the need for a comparison.
+"""
